4533e9afab6c215389241105c67a355d9820d0fa
davireisx <davireis530@gmail.com>
2024-03-01 16:45:48 -0300

    exercicio 1
    meu nome, idade e turma

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string nome = "Meu nome é Davi tenho 17 anos e sou da Turma 34 - Desenvolvimento de Sistemas.";
+    cout<< nome <<endl;
+
+    return 0;
+}
